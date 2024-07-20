926f162396907646a5d399876496d2ff3b1bc4ba
omar <202027471@std.sci.cu.edu.eg>
2024-07-20 20:45:05 +0300

    handle font scale

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,30 @@ class _MyAppState extends State<MyApp> {
           seedColor: Colors.white70,
           primary: Colors.blueGrey,
         ),
-
         useMaterial3: true,
       ),
+      builder: (context, child) {
+        // Retrieve the MediaQueryData from the current context.
+        final mediaQueryData = MediaQuery.of(context);
+
+        // Calculate the scaled text factor using the clamp function to ensure it stays within a specified range.
+        final scale = mediaQueryData.textScaler.clamp(
+          minScaleFactor: 1.0, // Minimum scale factor allowed.
+          maxScaleFactor: 1.3, // Maximum scale factor allowed.
+        );
+
+        // Create a new MediaQueryData with the updated text scaling factor.
+        // This will override the existing text scaling factor in the MediaQuery.
+        // This ensures that text within this subtree is scaled according to the calculated scale factor.
+        return MediaQuery(
+          // Copy the original MediaQueryData and replace the textScaler with the calculated scale.
+          data: mediaQueryData.copyWith(
+            textScaler: scale,
+          ),
+          // Pass the original child widget to maintain the widget hierarchy.
+          child: child!,
+        );
+      },
       home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
