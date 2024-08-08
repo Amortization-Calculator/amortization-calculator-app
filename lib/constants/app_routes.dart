6840d68200acb0fb1a7b5fb1dec0db691b868c17
omar <202027471@std.sci.cu.edu.eg>
2024-08-09 01:13:39 +0300

    update routes

--- lib/constants/app_routes.dart
+++ lib/constants/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:amortization_calculator_app/features/auth/screens/splash_screen.dart';
+import 'package:amortization_calculator_app/features/mortgage/screens/mortgage_screen.dart';
 import 'package:get/get.dart';
 import 'package:amortization_calculator_app/features/home/screens/home_screen.dart';
 import 'package:amortization_calculator_app/features/leasing/screens/result_screen.dart';
@@ -9,7 +10,7 @@ import '../features/auth/screens/register_screen.dart';
 
 class AppRoutes {
   static final routes = [
-    GetPage(name: '/', page: () => SplashScreen()),
+    GetPage(name: '/', page: () => const SplashScreen()),
     GetPage(name: '/NoInternetScreen', page: () => const NoInternetScreen()),
     //----------------------------------------------------------------------
     GetPage(name: '/LoginScreen', page: () => const LoginScreen()),
@@ -20,7 +21,7 @@ class AppRoutes {
     GetPage(name: '/LeasingScreen', page: () => LeasingScreen()),
     GetPage(name: '/ResultScreen', page: () => const ResultScreen()),
     //----------------------------------------------------------------------
-
+    GetPage(name: '/MortgageScreen', page: () => const MortgageScreen()),
 
   ];
 }
