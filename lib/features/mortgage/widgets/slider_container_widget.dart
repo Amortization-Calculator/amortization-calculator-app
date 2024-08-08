5c5c7d914093057b8206fee0e7b786fd28af06ba
omar <202027471@std.sci.cu.edu.eg>
2024-08-09 01:15:59 +0300

    add slider widget

---
+++ lib/features/mortgage/widgets/slider_container_widget.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+import '../../../widgets/slider_widget.dart';
+
+class SliderContainerWidget extends StatelessWidget {
+  final double sliderValue;
+  final Function(double) onValueChanged;
+
+  const SliderContainerWidget({
+    super.key,
+    required this.sliderValue,
+    required this.onValueChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 1,
+            offset: const Offset(0, 0),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Text(
+                'Duration',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                '${sliderValue.round()} years',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          SliderWidget(
+            onValueChanged: onValueChanged,
+          ),
+        ],
+      ),
+    );
+  }
+}
