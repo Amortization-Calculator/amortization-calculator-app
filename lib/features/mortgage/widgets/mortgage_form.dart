f5150544b5f3c4bc57597d0e31a83d0c37189161
omar <202027471@std.sci.cu.edu.eg>
2024-08-09 14:55:53 +0300

    solve controller bug

--- lib/features/mortgage/widgets/mortgage_form.dart
+++ lib/features/mortgage/widgets/mortgage_form.dart
@@ -11,7 +11,7 @@ class MortgageForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.find<MortgageController>();
+    final controller = Get.put(MortgageController());
 
     return Form(
       key: controller.formKey,
