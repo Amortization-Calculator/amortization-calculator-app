4a31fe96e05abfabf517a08073ac61bdc2470d44
omar <202027471@std.sci.cu.edu.eg>
2024-08-13 06:22:48 +0300

    add simple dashboard

--- lib/features/dashboard/screens/dashboard_screen.dart
+++ lib/features/dashboard/screens/dashboard_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../widgets/custom_appBar_widget.dart';
+import '../../../widgets/custom_divider_widget.dart';
+import '../../home/widgets/welcome_text_widget.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -9,7 +11,19 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: CustomAppBar(),
-//      body:
+     body:
+     Padding(
+       padding: EdgeInsets.all(16.0),
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           WelcomeTextWidget(),
+           SizedBox(height: 20),
+           CustomDividerWidget(),
+         ],
+       ),
+     ),
+
     );
   }
 }
